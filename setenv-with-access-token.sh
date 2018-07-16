5c9c8662de16b06ca804f54bdcba77ad2aff8394
Daniel Teixeira <ddtxra@gmail.com>
2018-07-17 01:16:01 +0200

    adding new line

--- setenv-with-access-token.sh
+++ setenv-with-access-token.sh
@@ -4,6 +4,8 @@ set -e
 python auth.py
 
 export ACCESS_TOKEN="$(grep -oE "(?:\"access_token\":\s?\")(.*?)(?:\")" tokens.json | cut -c 18- | rev | cut -c 2- | rev)"
+
+echo ""
 echo "Your access token is set in ACCESS_TOKEN environment variable, and can be used as following:"
 echo "> curl -H \"Authorization: Bearer \$ACCESS_TOKEN\" \"http://localhost:8080/my-protected-resource\""
 
